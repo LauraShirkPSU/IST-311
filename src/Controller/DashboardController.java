6b44c26b17b8849c07ec478ae548f1972bb6f783
Laura Shirk <llb5016@psu.edu>
2022-04-01 16:47:55 -0400

    added quick view of funds on dashboard

--- src/Controller/DashboardController.java
+++ src/Controller/DashboardController.java
@@ -1,15 +1,18 @@
 package Controller;
 
+import Model.ExpenseDAO;
+import Model.IncomeDAO;
 import java.io.IOException;
 import java.net.URL;
 import java.sql.SQLException;
 import java.util.ResourceBundle;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
-import javafx.scene.control.TableView;
-
+import javafx.scene.control.Label;
 
 
 public class DashboardController implements Initializable
@@ -19,15 +22,47 @@ public class DashboardController implements Initializable
     private Button btnBudgetCategories;
     @FXML
     private Button btnLogout;
-    private TableView tableView = new TableView<>();
-    
-    
+    @FXML
+    private Button btnIncome;
+    @FXML
+    private Button btnManageAcct;
+    @FXML
+    private Button btnExpenses;
+    @FXML
+    private Button btnReports;
+    @FXML
+    private Button btnNotifications;
+    @FXML
+    private Label label_TotalExpenses;
+    @FXML
+    private Label label_TotalIncome;
+    @FXML
+    private Label label_AvailableToSpend;
+     
+ 
     @Override
     public void initialize(URL url, ResourceBundle rb)
     {
-        // TODO
+        Double availableToSpend = 0.00;
+        try {
+            IncomeDAO incObject = new IncomeDAO();
+            label_TotalIncome.setText("Total Income to Date:         " + incObject.sumIncome().toString());
+            availableToSpend = availableToSpend + incObject.sumIncome();
+        } catch (SQLException ex) {
+            Logger.getLogger(DashboardController.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        try {
+            ExpenseDAO expObject = new ExpenseDAO();
+            label_TotalExpenses.setText("Total Expenses to Date:         " + expObject.sumExpenses().toString());
+            availableToSpend = availableToSpend - expObject.sumExpenses();
+        } catch (SQLException ex) {
+            Logger.getLogger(DashboardController.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        label_AvailableToSpend.setText("Funds available to spend:      " + availableToSpend.toString());
+        
     }    
-
+    
+    
     @FXML
     private void callBudgetCategories(ActionEvent event) throws SQLException, IOException 
     {    
@@ -41,6 +76,37 @@ public class DashboardController implements Initializable
     {   
         SceneController sc = new SceneController();
         sc.switchToLoginScene(event);
+    }
+
+    @FXML
+    private void manageIncome(ActionEvent event) throws IOException
+    {
+        SceneController sc = new SceneController();
+        sc.switchToIncomeScene(event);
+    }
+
+    @FXML
+    private void manageAccount(ActionEvent event) throws IOException
+    {  
+        SceneController sc = new SceneController();
+        sc.switchToManageAccountScene(event);
+    }
+
+    @FXML
+    private void manageExpense(ActionEvent event) throws IOException
+    {
+        SceneController sc = new SceneController();
+        sc.switchToExpenseScene(event);
+    }
+
+    @FXML
+    private void manageReports(ActionEvent event)
+    {
+    }
+
+    @FXML
+    private void manageNotifications(ActionEvent event)
+    {
     }
 
     
